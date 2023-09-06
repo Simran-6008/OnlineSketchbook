ea854a20d4d96bb99edba8ab6568600734aa4b48
Rubi <rubipra812@gmail.com>
2023-09-06 11:17:59 +0530

    DOM Manipulation

---
+++ sketch.js
@@ -0,0 +1,8 @@
+var divid = document.querySelector("#box1")
+divid.style.backgroundColor = "black";
+
+var divid = document.querySelector("#box16")
+divid.style.backgroundColor = "red";
+
+var divid = document.querySelector("#box10")
+divid.style.backgroundColor = "white";
