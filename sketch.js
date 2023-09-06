9ab81d3c8893d9d25695d66d189b3566c6cde297
Rubi <rubipra812@gmail.com>
2023-09-06 16:47:23 +0530

    Done DOM Events...

--- sketch.js
+++ sketch.js
@@ -1,9 +1,9 @@
-var divid = document.querySelector("#box1")
-divid.style.backgroundColor = "black";
+// var divid = document.querySelector("#box1")
+// divid.style.backgroundColor = "black";
 
 
-var divid = document.querySelector("#box16")
-divid.style.backgroundColor = "red";
+// var divid = document.querySelector("#box16")
+// divid.style.backgroundColor = "red";
 //divid.innerHTML="<p>Hello</p>"
 
 
@@ -12,19 +12,28 @@ divid.style.backgroundColor = "white";
 
 var container = document.querySelector(".container");
 container.setAttribute("id", "box17");
-container.style.backgroundColor = "pink";
+container.style.backgroundColor = "white";
 console.log(container.getAttribute("class"));
 
-var newdiv = document.createElement("div");
-newdiv.classList.add("divid");
-newdiv.textContent="B";
+// var newdiv = document.createElement("div");
+// newdiv.classList.add("divid");
+// newdiv.textContent="B";
 //newDiv.style.backgroundColor = "blue"
-container.appendChild(newdiv);
+// container.appendChild(newdiv);
 
 var totalnd = document.querySelectorAll(".divid");
 console.log(totalnd);
 
+//........................DOM EVENT...........................
+// divid.addEventListener("click", changeToBlack);
+// function changeToBlack(){
+// divid.style.backgroundColor="black";
+// }
 
-
+totalnd.forEach((div) => {
+    div.addEventListener('click', () =>{
+        div.style.backgroundColor = "black";
+    })
+})
 
 
