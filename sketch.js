88a7b3ff36d98ce922ec0a89cb88ac23acd62423
Rubi <rubipra812@gmail.com>
2023-09-06 14:36:04 +0530

    Done DOM Meniplation

--- sketch.js
+++ sketch.js
@@ -1,8 +1,30 @@
 var divid = document.querySelector("#box1")
 divid.style.backgroundColor = "black";
 
+
 var divid = document.querySelector("#box16")
 divid.style.backgroundColor = "red";
+//divid.innerHTML="<p>Hello</p>"
+
 
 var divid = document.querySelector("#box10")
 divid.style.backgroundColor = "white";
+
+var container = document.querySelector(".container");
+container.setAttribute("id", "box17");
+container.style.backgroundColor = "pink";
+console.log(container.getAttribute("class"));
+
+var newdiv = document.createElement("div");
+newdiv.classList.add("divid");
+newdiv.textContent="B";
+//newDiv.style.backgroundColor = "blue"
+container.appendChild(newdiv);
+
+var totalnd = document.querySelectorAll(".divid");
+console.log(totalnd);
+
+
+
+
+
